aa0add893b9fefa7a87c32ec8f692056233d5efc
Tim Mahmudov <tim.mahmudov@omnichains.com>
2024-07-24 19:39:11 -0500

    Auth Guard and routing

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { MemberListComponent } from './components/members/member-list/member-lis
 import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MemberListComponent},
-    {path: 'member/:id', component: MemberDetailComponent},
-    {path: 'lists', component: ListsComponent},
-    {path: 'messages', component: MessagesComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members', component: MemberListComponent},
+            {path: 'member/:id', component: MemberDetailComponent},
+            {path: 'lists', component: ListsComponent},
+            {path: 'messages', component: MessagesComponent},
+        ]
+    },
     {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
--- angular/src/app/components/navbar/navbar.component.ts
+++ angular/src/app/components/navbar/navbar.component.ts
@@ -4,11 +4,12 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ReactiveFormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
+  imports: [ReactiveFormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
