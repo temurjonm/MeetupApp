c7eedf0811e53f26ce1fde9445bd6b53dd5b4d32
Tim Mahmudov <tim.mahmudov@omnichains.com>
2024-07-31 17:03:02 -0500

    Member Edit

--- angular/src/app/components/members/member-edit/member-edit.component.ts
+++ angular/src/app/components/members/member-edit/member-edit.component.ts
@@ -1,27 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { AccountService } from '../../../services/account.service';
 import { MemberService } from '../../../services/member.service';
 import { Observable } from 'rxjs';
 import { IMember } from '../../../models/members.model';
 import { AsyncPipe, DatePipe, NgIf } from '@angular/common';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
   standalone: true,
-  imports: [AsyncPipe, NgIf, TabsModule, DatePipe],
+  imports: [AsyncPipe, NgIf, TabsModule, DatePipe, FormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.scss'
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm") editForm?: NgForm;
   private _accountService = inject(AccountService);
   private _memberService = inject(MemberService);
+  private _toastr = inject(ToastrService);
+
   member$!: Observable<IMember>;
 
   ngOnInit(): void {
     const user = this._accountService.currentUser();
     if (!user) return;
     this.member$ = this._memberService.getMember(user.username);
+  }
+
+  updateMember() {
+    this._toastr.success("Profile updated successfully!");
+    this.editForm?.reset();
   }
 
 }
